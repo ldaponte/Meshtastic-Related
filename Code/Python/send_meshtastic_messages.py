eb997901274da4712e48cb3a240872260ba85f7c
Larry da Ponte <larry.daponte@gmail.com>
2025-01-26 14:01:32 -0800

    Getting reconnect to work

--- Code/Python/send_meshtastic_messages.py
+++ Code/Python/send_meshtastic_messages.py
@@ -4,17 +4,46 @@ import time
 import meshtastic
 import meshtastic.tcp_interface
 from pubsub import pub
+import threading
+
+
+checkInterfaceTimer = None
+
+def onCheckInterface(interface):
+    print("checking interface")
+    print("is connected:", interface.isConnected.is_set())
+    interface.sendHeartbeat()
+    global checkInterfaceTimer
+    checkInterfaceTimer = None
+    checkInterfaceTimer = threading.Timer(10, onCheckInterface, args=(interface,))
+    checkInterfaceTimer.start()
 
 def onReceive(packet, interface): # called when a packet arrives
-
     mqtt_message = meshtastic.util.message_to_json(packet["raw"], multiline=True)
-
-    mqtt_client.publish(mqtt_topic, mqtt_message)
+    print("received message:", len(mqtt_message))
+    # mqtt_client.publish(mqtt_topic, mqtt_message)
 
 def onConnection(interface, topic=pub.AUTO_TOPIC): # called when we (re)connect to the radio
     # defaults to broadcast, specify a destination ID if you wish
     # interface.sendText("hello mesh")
+    print("connected")
     noop = True
+
+def onConnectionLost(interface):
+
+    print("lost connection")
+
+    while True:
+        try:
+            interface = meshtastic.tcp_interface.TCPInterface(hostname=meshtastic_host)
+            # interface.heartbeatTimer.interval = 10
+            # checkInterfaceTimer = threading.Timer(10, onCheckInterface, args=(interface,))
+            # checkInterfaceTimer.start()
+            break
+        except Exception as e:
+            print("error:", e)
+            print("type:", type(e))
+            time.sleep(60)
 
 meshtastic_host = os.getenv('MESHTASTIC_HOST')
 mqtt_host = os.getenv('MQTT_HOST')
@@ -29,7 +58,21 @@ mqtt_client.connect(mqtt_host,  1883, 60)  # Replace with your broker's address 
 
 pub.subscribe(onReceive, "meshtastic.receive")
 pub.subscribe(onConnection, "meshtastic.connection.established")
-interface = meshtastic.tcp_interface.TCPInterface(hostname=meshtastic_host)
+pub.subscribe(onConnectionLost, "meshtastic.connection.lost")
+interface = None
+
+while True:
+    try:
+        interface = meshtastic.tcp_interface.TCPInterface(hostname=meshtastic_host)
+        # interface.heartbeatTimer.interval = 10
+
+        break
+    except Exception as e:
+        print("error:", e)
+        print("type:", type(e))
+
+# checkInterfaceTimer = threading.Timer(10, onCheckInterface, args=(interface,))
+# checkInterfaceTimer.start()
 
 while True:
     time.sleep(1000)
